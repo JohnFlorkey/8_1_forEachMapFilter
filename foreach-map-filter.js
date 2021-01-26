402a60cc41c3c916feb774fac96b42cfed4e68c1
John Florkey <john.florkey@gmail.com>
2021-01-25 19:46:24 -0500

    findInObj working

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -184,7 +184,12 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+    const newArr = arr.filter(function(value){
+        return value[key] === searchValue;
+    });
+    return newArr[0];
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
