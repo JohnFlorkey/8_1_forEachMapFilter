5b5461c5d10a95802969aa456a3002d477f01683
John Florkey <john.florkey@gmail.com>
2021-01-25 19:27:02 -0500

    valTimesIndex working

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -118,8 +118,10 @@ Examples:
 */
 
 function valTimesIndex(arr){
-    
-}
+    return arr.map(function(value, index){
+        return value * index;
+    });
+};
 
 /*
 Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
